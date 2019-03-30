2738eb0d8c48985cefa9b1455b13755f7c9ec62d
xiaolin <462441355@qq.com>
2019-03-30 15:56:01 +0800

    * 增加redis 操作

--- analysis/main.go
+++ analysis/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"bufio"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
 	"fmt"
+	"github.com/mediocregopher/radix.v2/pool"
+	"github.com/mgutz/str"
 	"io"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"time"
-	"github.com/mgutz/str"
 )
 
 const HANDLE_DIG  = " /dig?"
+const HANDLE_MOVIE = "/movie/"
+const HANDLE_LIST = "/list/"
+const HANDLE_HTML  = ".html"
 
 type cmdParams struct {
 	logFilePath string
@@ -28,10 +36,14 @@ type digData struct {
 type urlData struct {
 	data digData
 	uid string
+	unode  urlNode
 }
 
 type urlNode struct {
-
+	unType string
+	unRid int
+	unUrl string
+	unTime string
 }
 
 type storageBlock struct {
@@ -41,6 +53,7 @@ type storageBlock struct {
 }
 
 func init()  {
+
 }
 
 // 获取参数->打日志->初始化channel->日志消费->创建一组日志处理->创建pv uv->创建存储器
@@ -59,6 +72,19 @@ func main() {
 	uvChannel := make(chan urlData, params.routineNum)
 	storageChannel := make(chan storageBlock, params.routineNum)
 
+	// Redis Pool
+	redisPool, err := pool.New( "tcp", "127.0.0.1:6379", 2*params.routineNum )
+	if err != nil{
+		fmt.Printf( "Redis pool created failed." )
+		panic(err)
+	} else {
+		go func() {
+			for {
+				redisPool.Cmd("PING")
+				time.Sleep(3 * time.Second)
+			}
+		}()
+	}
 	// 日志消费者
 	go readFileLineByLine(params, logChannel)
 
@@ -69,7 +95,7 @@ func main() {
 
 	// 创建pv uv统计
 	go pvCounter(pvChannel, storageChannel)
-	go uvCounter(uvChannel, storageChannel)
+	go uvCounter(uvChannel, storageChannel, redisPool)
 
 	// 创建存储器
 	go dataStorage(storageChannel)
@@ -84,17 +110,44 @@ func dataStorage(storageChannel chan storageBlock)  {
 }
 
 func pvCounter(pvChannel chan urlData, storageChannel chan storageBlock)  {
-
+	for data := range pvChannel{
+		sItem := storageBlock{ "pv", "ZINCRBY", data.unode }
+		storageChannel <- sItem
+	}
 }
 
-func uvCounter(pvChannel chan urlData, storageChannel chan storageBlock)  {
+func uvCounter(uvChannel chan urlData, storageChannel chan storageBlock, redisPool *pool.Pool)  {
+	for data := range uvChannel {
+		//HyperLoglog redis
+		hyperLogLogKey := "uv_hpll_" + getTime(data.data.time, "day")
+		ret, err := redisPool.Cmd("PFADD", hyperLogLogKey, data.uid, "EX", 86400).Int()
+		if err != nil {
+			fmt.Printf("UvCounter check redis hyperloglog failed, ", err)
+		}
+		if ret != 1 {
+			continue
+		}
 
+		sItem := storageBlock{"uv", "ZINCRBY", data.unode}
+		storageChannel <- sItem
+	}
 }
 
 func logConsumer(logChannel chan string, pvChannel chan urlData, uvChannel chan urlData) error {
 	for logStr := range logChannel {
 		// 切割日志
 		data := cutLogFetchData(logStr)
+
+		hasher := md5.New()
+		hasher.Write([]byte(data.refer + data.ua))
+		uid := hex.EncodeToString(hasher.Sum(nil))
+		uData := urlData{
+			data,
+			uid,
+			formatUrl(data.url,data.time),
+		}
+		pvChannel <- uData
+		uvChannel <- uData
 	}
 
 	return nil
@@ -132,9 +185,70 @@ func readFileLineByLine(params cmdParams, logChannel chan string) error {
 	return nil
 }
 
+// 解析url
 func cutLogFetchData(log string) digData {
 	log = strings.TrimSpace(log)
-	//str.In
+	pos1 := str.IndexOf(log, HANDLE_DIG, 0)
+	if pos1 == -1 {
+		return digData{}
+	}
+	pos1 += len(HANDLE_DIG)
 
+	pos2 := str.IndexOf(log, " HTTP/", pos1)
+	d := str.Substr(log, pos1, pos2 - pos1)
+
+	urlInfo, err := url.Parse("http://localhost/?" + d)
+	if err != nil {
+		return digData{}
+	}
+	data := urlInfo.Query()
+	return digData{
+		data.Get("time"),
+		data.Get("url"),
+		data.Get("refer"),
+		data.Get("ua"),
+	}
+
+
+}
+
+func formatUrl(url, t string) urlNode {
+	pos1 := str.IndexOf( url, HANDLE_MOVIE, 0)
+	if pos1!=-1 {
+		pos1 += len( HANDLE_MOVIE )
+		pos2 := str.IndexOf( url, HANDLE_HTML, 0 )
+		idStr := str.Substr( url , pos1, pos2-pos1 )
+		id, _ := strconv.Atoi( idStr )
+		return urlNode{ "movie", id, url, t }
+	} else {
+		pos1 = str.IndexOf( url, HANDLE_LIST, 0 )
+		if pos1!=-1 {
+			pos1 += len( HANDLE_LIST )
+			pos2 := str.IndexOf( url, HANDLE_HTML, 0 )
+			idStr := str.Substr( url , pos1, pos2-pos1 )
+			id, _ := strconv.Atoi( idStr )
+			return urlNode{ "list", id, url, t }
+		} else {
+			return urlNode{ "home", 1, url, t}
+		} // 如果页面url有很多种，就不断在这里扩展
+	}
+}
+
+
+func getTime( logTime, timeType string ) string {
+	var item string
+	switch timeType {
+	case "day":
+		item = "2018-01-02"
+		break
+	case "hour":
+		item = "2018-01-02 15"
+		break
+	case "min":
+		item = "2018-01-02 15:04"
+		break
+	}
+	t, _ := time.Parse(item, time.Now().Format(item))
+	return strconv.FormatInt(t.Unix(), 10)
 
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module go_log
 
 go 1.12
 
-require github.com/mgutz/str v1.2.0
+require (
+	github.com/mediocregopher/radix.v2 v0.0.0-20181115013041-b67df6e626f9
+	github.com/mgutz/str v1.2.0
+	github.com/stretchr/testify v1.3.0 // indirect
+)
