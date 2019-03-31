2204aecd3beb2c66da04bfbb8b4f098bffae668b
xiaolin <462441355@qq.com>
2019-04-01 01:07:09 +0800

    * 数据库配置提取

--- collect_log/dbops/conn.go
+++ collect_log/dbops/conn.go
@@ -2,16 +2,28 @@ package dbops
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
+)
+
+const (
+	USERNAME = "root"
+	PASSWORD = "root"
+	NETWORK  = "tcp"
+	SERVER   = "localhost"
+	PORT     = 3306
+	DATABASE = "go_video"
 )
 
 var (
 	dbConn *sql.DB
-	err error
+	err    error
 )
 
 func init() {
-	dbConn, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/go_video?charset=utf8")
+	dsn := fmt.Sprintf("%s:%s@%s(%s:%d)/%s?charset=utf8", USERNAME, PASSWORD, NETWORK, SERVER, PORT, DATABASE)
+	dbConn, err = sql.Open("mysql", dsn)
+	//dbConn, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/go_video?charset=utf8")
 	if err != nil {
 		panic(err.Error())
 	}
